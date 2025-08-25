585718ba83c4d1fdaec5d09584878aa81574f25a
LMAlieen <2277486684@qq.com>
2025-08-25 16:22:42 +0800

    unitig-counter change1

--- recipes/unitig-counter/build.sh
+++ recipes/unitig-counter/build.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
-# build unitig-counter
-mkdir build && pushd build
-cmake -DCMAKE_BUILD_TYPE=Release -DBoost_INCLUDE_DIR=${PREFIX}/include -DBoost_LIBRARY_DIR=${PREFIX}/lib -DCMAKE_INSTALL_PREFIX=$PREFIX ..
-make VERBOSE=1
+
 install -d $PREFIX/bin
-install unitig-counter cdbg-ops ext/gatb-core/bin/Release/gatb-h5dump $PREFIX/bin
-popd
+
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+export CFLAGS="${CFLAGS} -O3 -I${PREFIX}/include"
+if [[ `uname -s` == "Darwin" ]]; then
+    export CFLAGS="${CFLAGS} -Wno-error=int-conversion -Wno-error=implicit-function-declaration"
+    export CXXFLAGS="${CXXFLAGS} -Wno-error=int-conversion"
+fi
+
+sed -i.bak 's|Boost_USE_STATIC_LIBS   ON|Boost_USE_STATIC_LIBS   OFF|' CMakeLists.txt
+sed -i.bak 's|-Wnested-externs -Winline|-Wnested-externs -Winline -Wno-int-conversion -Wno-implicit-function-declaration|' gatb-core/gatb-core/thirdparty/hdf5/config/cmake/HDFCompilerFlags.cmake
+rm -rf *.bak
+
+if [[ `uname -s` == "Darwin" ]]; then
+	export CONFIG_ARGS="-DCMAKE_FIND_FRAMEWORK=NEVER -DCMAKE_FIND_APPBUNDLE=NEVER"
+else
+	export CONFIG_ARGS=""
+fi
+
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release \
+	-DGATB_CORE_USE_SYSTEM_HDF5=ON \
+	-DBOOST_ROOT="${PREFIX}" \
+	-DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+	-DCMAKE_C_COMPILER="${CC}" \
+	-DCMAKE_C_FLAGS="${CFLAGS} -std=c11" \
+	-DCMAKE_CXX_FLAGS="${CXXFLAGS} -std=c++11" \
+	-Wno-dev -Wno-deprecated --no-warn-unused-cli \
+	"${CONFIG_ARGS}"
+cmake --build build -j "${CPU_COUNT}"
+
+cd build
+install -v -m 0755 unitig-counter cdbg-ops ext/gatb-core/bin/Release/gatb-h5dump $PREFIX/bin
